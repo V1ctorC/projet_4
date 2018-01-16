f7895d36d3789a0aa8eb26a03eedba97a6d05d73
VictorC <victor.du.77@hotmail.fr>
2018-01-16 16:38:11 +0100

    Correction de getComments => user_id

--- model/model.php
+++ model/model.php
@@ -24,7 +24,7 @@ function getComments($postId)
 {
 
 	$db = dbConnect();
-	$comments = $db->prepare('SELECT comment_id, comment_content, comment_date, id, user_name FROM comments WHERE id = ? ORDER BY comment_date DESC');
+	$comments = $db->prepare('SELECT comment_id, comment_content, comment_date, id, user_id FROM comments WHERE id = ? ORDER BY comment_date DESC');
 	$comments->execute(array($postId));
 
 	return $comments;
@@ -34,7 +34,7 @@ function getComments($postId)
 function postComment($postId, $author, $comment)
 {
     $db = dbConnect();
-    $comments = $db->prepare('INSERT INTO comments(id, user_name, comment, comment_date) VALUES(?, ?, ?, NOW())');
+    $comments = $db->prepare('INSERT INTO comments(id, user_name, comment_content, comment_date) VALUES(?, ?, ?, NOW())');
     $affectedLines = $comments->execute(array($postId, $author, $comment));
 
     return $affectedLines;
