b18ef3bb948d3b553d675b98b71a7eb54ff3041f
VictorC <victor.du.77@hotmail.fr>
2018-01-16 16:20:02 +0100

    Ajout fonction postComment au model

--- model/model.php
+++ model/model.php
@@ -24,11 +24,20 @@ function getComments($postId)
 {
 
 	$db = dbConnect();
-	$comments = $db->prepare('SELECT comment_id, comment_content, comment_date, id, user_id FROM comments WHERE id = ? ORDER BY comment_date DESC');
+	$comments = $db->prepare('SELECT comment_id, comment_content, comment_date, id, user_name FROM comments WHERE id = ? ORDER BY comment_date DESC');
 	$comments->execute(array($postId));
 
 	return $comments;
 
+}
+
+function postComment($postId, $author, $comment)
+{
+    $db = dbConnect();
+    $comments = $db->prepare('INSERT INTO comments(id, user_name, comment, comment_date) VALUES(?, ?, ?, NOW())');
+    $affectedLines = $comments->execute(array($postId, $author, $comment));
+
+    return $affectedLines;
 }
 
 function dbConnect()
