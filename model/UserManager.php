b5e05bf8a48d72ab067c96dbd4cd4b8f77573c5e
VictorC <victor.du.77@hotmail.fr>
2018-01-19 15:31:13 +0100

    Ajout de la requete pour la creation de membre => createUser

---
+++ model/UserManager.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('model/manager.php');
+
+class UserManager extends Manager
+{
+	
+	public function createUser($mail, $pseudo, $password) 
+	{
+		$db = $this->dbConnect();
+		$req = $db->prepare('INSERT INTO user(user_mail, user_pseudo, user_password VALUES (?, ?, ?)');
+		$affectedLines = $req->execute(array($mail, $pseudo, $password));
+
+		return $affectedLines;
+	}
+}
