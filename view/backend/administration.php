6b212cf0c1024c9774dae2b6dd8770814ef60919
VictorC <victor.du.77@hotmail.fr>
2018-01-26 14:36:10 +0100

    Ajout de CSS pour la partie backend + debut de mise en place du HTaccess

--- view/backend/administration.php
+++ view/backend/administration.php
@@ -3,7 +3,15 @@
 <?php ob_start(); ?>
 <h1>Administration du Blog</h1>
 
-<p>Vous êtes sur la partie administration du blog</p>
+<div id="content">
+    <div id="posts">
+        <h2>posts</h2>
+    </div>
+    <div id="comments">
+        <h2>comments</h2>
+    </div>
+    
+</div>
 
 
 <?php $content = ob_get_clean(); ?>
