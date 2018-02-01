66942304c8c7abf54ce6d6d74223ead864caf573
VictorC <victor.du.77@hotmail.fr>
2018-02-01 11:44:43 +0100

    Mise en place du Htaccess et du Htpasswrd

--- view/backend/template.php
+++ view/backend/template.php
@@ -9,7 +9,7 @@
 		</script>
         <meta charset="utf-8" />
         <title><?= $title ?></title>
-        <link href="../../public/css/backend.css" rel="stylesheet" />
+        <link href="public/css/backend.css" rel="stylesheet" />
     </head>
         
     <body>
